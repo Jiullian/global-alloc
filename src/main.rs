eede16c27bba578af1a6526431b7bcfbfc72f7d8
Jiullian Ferreira <jiullian77470@gmail.com>
2024-10-21 22:59:47 +0200

    feat - configuration ready

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+use core::alloc::{GlobalAlloc, Layout};
+use core::ptr::null_mut;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
 }
